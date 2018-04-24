433f3ab39db9e1c61fb6ebd7c3414afb1f61ae3c
Mike <mikefmcnalley@gmail.com>
2018-04-24 14:59:55 -0700

    good

---
+++ app/src/main/java/com/example/mike/myapplication/Constants.java
@@ -0,0 +1,9 @@
+package com.example.mike.myapplication;
+
+class Constants {
+    static final String KEY_NAME = "name";
+    static final String KEY_Username = "username";
+    static final String KEY_Age = "age";
+    static final String KEY_DOB = "DOB";
+    static final String KEY_EMAIL = "Email";
+}
